aa499d6ce66e4a776d87a4144de16e89ccdd2ca9
AppInstalleriOSGH <98632439+BenjaminHornbeck6@users.noreply.github.com>
2026-03-25 21:32:23 -0400

    Stuff

--- src/cli/main.c
+++ src/cli/main.c
@@ -15,42 +15,48 @@ int main(int argc, char *argv[]) {
 			printf("Kernel entry: 0x%llx\n", gXPF.kernelEntry);
 			//xpf_print_all_items();
 
-			char *sets[] = {
-				"translation",
-				"trustcache",
-				"physmap",
-				"struct",
-				"physrw",
-				NULL,
-				NULL,
-				NULL,
-				NULL,
-				NULL,
-				NULL,
-			};
+//			char *sets[] = {
+//				"translation",
+//				"trustcache",
+//				"physmap",
+//				"struct",
+//				"physrw",
+//				NULL,
+//				NULL,
+//				NULL,
+//				NULL,
+//				NULL,
+//				NULL,
+//			};
+//
+//			uint32_t idx = 0;
+//			while (sets[idx] != NULL) idx++;
+//
+//			if (xpf_set_is_supported("sandbox")) {
+//				sets[idx++] = "sandbox";
+//			}
+//			if (xpf_set_is_supported("perfkrw")) {
+//				sets[idx++] = "perfkrw";
+//			}
+//			if (xpf_set_is_supported("devmode")) {
+//				sets[idx++] = "devmode"; 
+//			}
+//			if (xpf_set_is_supported("badRecovery")) {
+//				sets[idx++] = "badRecovery"; 
+//			}
+//			if (xpf_set_is_supported("arm64kcall")) {
+//				sets[idx++] = "arm64kcall"; 
+//			}
+//			if (xpf_set_is_supported("trigon")) {
+//				sets[idx++] = "trigon"; 
+//			}
 
-			uint32_t idx = 0;
-			while (sets[idx] != NULL) idx++;
-
-			if (xpf_set_is_supported("sandbox")) {
-				sets[idx++] = "sandbox";
-			}
-			if (xpf_set_is_supported("perfkrw")) {
-				sets[idx++] = "perfkrw";
-			}
-			if (xpf_set_is_supported("devmode")) {
-				sets[idx++] = "devmode"; 
-			}
-			if (xpf_set_is_supported("badRecovery")) {
-				sets[idx++] = "badRecovery"; 
-			}
-			if (xpf_set_is_supported("arm64kcall")) {
-				sets[idx++] = "arm64kcall"; 
-			}
-			if (xpf_set_is_supported("trigon")) {
-				sets[idx++] = "trigon"; 
-			}
-
+            char *sets[] = {
+                "translation",
+                "struct",
+                NULL,
+            };
+            
 			xpc_object_t serializedSystemInfo = xpf_construct_offset_dictionary((const char **)sets);
 			if (serializedSystemInfo) {
 				xpc_dictionary_apply(serializedSystemInfo, ^bool(const char *key, xpc_object_t value) {
